773ae949a4afb0a902b1ed329810caa46f426592
Eka Prasetia <ekaone3033@gmail.com>
2020-10-03 11:40:48 +0700

    add stuff

---
+++ additem.js
@@ -0,0 +1,10 @@
+const elements = [1, 2, 3];
+
+//[0, ...elements]
+
+elements.splice(3, 0, 4);
+console.log(elements);
+
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.splice(2, 0, "Lemon", "Kiwi");
+// console.log(fruits);
