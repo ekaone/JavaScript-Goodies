638785539a9633ce47a1b4f0a51dfb0121b9f6a5
Eka Prasetia <ekaone3033@gmail.com>
2020-11-09 09:08:21 +0700

    add fibonacci

---
+++ fibonacci.js
@@ -0,0 +1,26 @@
+// Time complexity: O(2^N)
+// function fibonacci(num) {
+//   if (num <= 1) {
+//     return 1;
+//   }
+
+//   return fibonacci(num - 1) + fibonacci(num - 2);
+// }
+
+// Time complexity: O(N)
+function fibonacci(num) {
+  var a = 1,
+    b = 0,
+    temp;
+
+  while (num >= 0) {
+    temp = a;
+    a = a + b;
+    b = temp;
+    num--;
+  }
+
+  return b;
+}
+
+console.log(fibonacci(40));
