f196344334f951ed7536cb99a609f2a9ee5794f8
ekaone <ekaone3033@gmail.com>
2021-03-12 07:25:05 +0700

    add useToggler

---
+++ useToggler.js
@@ -0,0 +1,14 @@
+const useToggler = (initialState) => {
+  const [value, setValue] = React.useState(initialState);
+
+  const toggleValue = React.useCallback(() => setValue((prev) => !prev), []);
+
+  return [value, toggleValue];
+};
+
+// Usage
+
+const Switch = () => {
+  const [val, toggleVal] = useToggler(false);
+  return <button onClick={toggleVal}>{val ? "ON" : "OFF"}</button>;
+};
