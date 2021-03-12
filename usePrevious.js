4425c1e15457589b38e4997060210926c898e129
ekaone <ekaone3033@gmail.com>
2021-03-12 07:26:02 +0700

    add usePrevious

---
+++ usePrevious.js
@@ -0,0 +1,23 @@
+const usePrevious = (value) => {
+  const ref = React.useRef();
+  React.useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+};
+
+// Usage
+
+const Counter = () => {
+  const [value, setValue] = React.useState(0);
+  const lastValue = usePrevious(value);
+
+  return (
+    <div>
+      <p>
+        Current: {value} - Previous: {lastValue}
+      </p>
+      <button onClick={() => setValue(value + 1)}>Increment</button>
+    </div>
+  );
+};
